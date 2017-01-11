0705a3c327587ef61919208aff96816effe2780f
alex_shulga <shulga777@gmail.com>
2017-01-11 19:18:12 +0200

    Hello World

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alexander Shulga on 11.01.2017.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+<h1>Hello World!!!</h1>,
+    document.getElementById('root')
+);
